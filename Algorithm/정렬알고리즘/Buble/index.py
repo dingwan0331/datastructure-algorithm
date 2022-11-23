68204409820f584fcca9bdbb69a87ba9d20bdf5a
dingwan <dingwan0331@gmail.com>
2022-11-23 18:02:57 +0900

    create: Buble sort

---
+++ Algorithm/정렬알고리즘/Buble/index.py
@@ -0,0 +1,8 @@
+def buble_sort(array):
+    n = len(array)
+
+    for i in range(n):
+        for j in range(1,n-i):
+            if array[j-1] > array[j]:
+                array[j-1], array[j] = array[j], array[j-1]
+    return array
