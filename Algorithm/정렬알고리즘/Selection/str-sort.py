68249a2f3c0551c9abe1ba0eec84487fa403a44e
dingwan <dingwan0331@gmail.com>
2022-11-23 17:19:20 +0900

    update: Selection sort

--- Algorithm/정렬알고리즘/Selection/str-sort.py
+++ Algorithm/정렬알고리즘/Selection/str-sort.py
@@ -1,8 +1,21 @@
+# 오름차순 정렬
 def str_selection_sort(string:str) -> str:
     n = len(string)
-    str_list = list(map(lambda x : ord(x),string))
-    answer = ''
-    
-    return str_list
+    str_list = list(map(ord,string))
 
-print(str_selection_sort('babsaa'))
+    for i in range(n-1):
+        max_idx = str_list.index(max(str_list[:n-1-i]))
+        str_list[n -1 -i],str_list[max_idx] = str_list[max_idx], str_list[n-1-i]
+
+    return ''.join(list(map(chr,str_list)))
+
+# 내림차순 정렬
+def str_selection_sort_desc(string:str)->str:
+    n = len(string)
+    str_list = list(map(ord,string))
+
+    for i in range(n-1):
+        min_idx = str_list.index(min(str_list[:n-1-i]))
+        str_list[n -1 -i],str_list[min_idx] = str_list[min_idx], str_list[n-1-i]
+        
+    return ''.join(list(map(chr,str_list)))
